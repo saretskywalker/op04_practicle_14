8515ab547fb5b0df1a7b38f7fbecfba7c691d5d4
saretskywalker <138796350+saretskywalker@users.noreply.github.com>
2026-01-12 11:37:45 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,12 +1,26 @@
 import random
 N = random.randint(5,10)
 list_of_numbers = []
+list_of_numbers_2 = []
 
 for i in range (N):
     list_of_numbers.append(random.randint(1,100))
 print(list_of_numbers)
-biggest_digit = list_of_numbers[0]
-for i in list_of_numbers:
-    if i > biggest_digit:
-        biggest_digit = i
-print(biggest_digit)
+
+for i in range (N):
+    list_of_numbers_2.append(random.randint(1,100))
+print(list_of_numbers_2)
+
+list_of_numbers = [list_of_numbers_2, list_of_numbers]
+print(list_of_numbers)
+
+biggest_digit = list_of_numbers[0][0]
+index_biggest_digit =f"{0} {list_of_numbers[0].index(biggest_digit)}"
+
+for i in range(len(list_of_numbers)):
+    for j in list_of_numbers[i]:
+        if j > biggest_digit:
+            biggest_digit = j
+            index_biggest_digit =f"{i} {list_of_numbers[i].index(biggest_digit)}"
+
+print(f"Наибольший элемент списка: {biggest_digit} позиция элемента в списке{index_biggest_digit}")
